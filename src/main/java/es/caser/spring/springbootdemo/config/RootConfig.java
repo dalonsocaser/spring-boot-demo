512101f173a47866b28ab6cf256171cea020da00
DANIEL ALONSO SANCHEZ <DALOSA01@caser.local>
2017-12-11 16:51:03 +0100

    quitado codigo no necesario por estar en spring boot

--- src/main/java/es/caser/spring/springbootdemo/config/RootConfig.java
+++
@@ -1,9 +0,0 @@
-package es.caser.spring.springbootdemo.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RootConfig {
-   //Service and Repository beans configuration
-   //....
-}
